0d3bf4299a25b2ebac634b701502c55b84aa8d55
elisadepedrini <elisa.depedrini@gmail.com>
2026-03-31 11:43:45 +0200

    Complete snack-5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,23 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+
+//FILTER
+const evenArray = nums.filter((number) => number % 2 === 0)
+
+console.log(evenArray);
+
+
+// FOREACH
+const newArray = []
+
+nums.forEach((number) => {
+    if (number % 2 === 0) {
+        newArray.push(number)
+    }
+})
+
+console.log(newArray);
+
+
 // Risultato: [2, 8, 4, 12]
